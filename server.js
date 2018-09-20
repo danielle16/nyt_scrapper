c6d44d5e3936414b7454c904495178bee6af8cb9
Dani G <daniellegarcia@MacBook-Air-3.attlocal.net>
2018-09-20 09:43:52 -0400

    .

--- server.js
+++ server.js
@@ -1,16 +1,67 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const handlebars = require("handlebars");
-const mongoose = require("mongoose");
-const cheerio = require("cheerio");
-const request = require("request");
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var request = require("request");
+
+var axios = require("axios");
+
+var router = express.Router();
+var db = require("./models/articles");
+var PORT = 3000;
 
 var app = express();
-var PORT = 3000; 
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nyt_scrapper";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true });
+
+
+
+
+// app.get("/", function (req, res) {
+//    res.send("hello world");
+// });
+
+//Scrape nyt articles 
+app.get("/scraped-articles", function (req, res) {
+
+    axios.get("https://www.nytimes.com/section/us").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $(".headline").each(function (i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+    
+      db.Article.create(result)
+      .then(function(dbArticle) {
+          res.json(dbArticle);
+        console.log(dbArticle);
+      })
+      .catch(function(error) {
+            console.log("An error occured")
+        });  
+
+    });
+    
+    res.send("Scrape Complete");
+    
+    }); 
+});
 
 
 
@@ -18,7 +69,8 @@ app.use(express.static("public"));
 
 
 
-app.listen(PORT, function() {
+
+app.listen(PORT, function () {
     console.log("Listening on port: " + PORT);
 });
 
